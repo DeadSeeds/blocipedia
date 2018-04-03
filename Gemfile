ae9771cb68c47a8f4151a4032aca2cb0ec40c09e
Joshua Brown <xxdeadseeds@gmail.com>
2018-04-02 19:04:27 -0700

    Checkpoint 2 complete

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@
 
  group :development do
    gem 'sqlite3'
+   gem 'letter_opener'
  end
 
  group :development, :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,6 @@ Rails.application.configure do
 
   #Devise configs
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener  # change back to :smtp
   config.action_mailer.default_url_options = { host: 'localhost' }
 end
